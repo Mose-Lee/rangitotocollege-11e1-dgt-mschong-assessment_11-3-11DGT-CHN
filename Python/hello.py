8082bb65e3fae796807f60b29103391855f94e4b
Mose-Lee <183690@cloud.rangitoto.school.nz>
2025-08-15 10:49:40 +1200

    update.py

--- Python/hello.py
+++ Python/hello.py
@@ -1,6 +1,27 @@
-print("Hello world!")
-while True:
-  name = input('Enter name: ')
-  if name == 'stop':
-    break
+from tkinter import *
+win = Tk()
+win.title("Mose")
+win.geometry("500x400")
+#label
+
+
+#entry
+entry = Entry(win, width=30,font=("Arial", 14))
+entry.pack()
+#def help function
+def help():
+    text = entry.get()
+    if text:
+        btn.config(text="You entered: " + text)
+        print("You entered:", text)
+    else:
+        print("please enter something")
+#button
+btn = Button(win)
+btn.config(text="Click Here")
+btn.config(command=help)
+btn.pack()
+
+
+win.mainloop()
   
